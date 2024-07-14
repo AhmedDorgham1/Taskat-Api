85f7c3c618cb361999379336750226754124308c
Ahmed Dorgham <ahmeddorgham779@gmail.com>
2024-07-15 01:33:05 +0300

    Add pagination to getAllTasks and create pagination utility

--- src/modules/task/task.controllers.js
+++ src/modules/task/task.controllers.js
@@ -1,7 +1,14 @@
 import Task from "../../../database/models/task.model.js";
+import { paginationFunction } from "../../utils/pagination.js";
 
 export const getAllTasks = catchAsyncError(async (req, res) => {
-  const tasks = await Task.find().populate("categoryId");
+  const { page, size } = req.query;
+  const { limit, skip } = paginationFunction({ page, size });
+
+  const tasks = await Task.find()
+    .populate("categoryId")
+    .limit(limit)
+    .skip(skip);
   res.json({ message: tasks });
 });
 
