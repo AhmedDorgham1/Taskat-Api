8d34818e31f910bdce5ceb14e6344c3acc3bc724
Ahmed Dorgham <ahmeddorgham779@gmail.com>
2024-07-15 01:14:11 +0300

    Some updates

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ process.on("uncaughtException", () => {
 
 const app = express();
 const port = 3000;
-
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/categories", categoryRouter);
